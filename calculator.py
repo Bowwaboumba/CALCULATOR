341af09c11c5034282a05476357782d383f5df86
Rafael <122923722+Bowwaboumba@users.noreply.github.com>
2023-06-10 22:55:41 +0200

    Add files via upload
    read 'readme' for how to use.

---
+++ calculator.py
@@ -0,0 +1,48 @@
+commencer = input("Appuyez sur enter pour commencer")
+print("Select an entry")
+print("1. Add")
+print("2. Substract")
+print("3. Multiply")
+print("4. Divide")
+
+operation = input()
+
+if operation == "1":
+    num1 = input("Enter the first number")
+    num2 = input("Enter the second number")
+    print("The sum is " + str(int(num1) + int(num2)))
+
+if operation == "2":
+    num1 = input("Enter the first number")
+    num2 = input("Enter the second number")
+    print("The difference is " + str(int(num1) - int(num2)))
+
+if operation == "3":
+    num1 = input("Enter the first number")
+    num2 = input("Enter the second number")
+    print("The product is " + str(int(num1) * int(num2)))
+
+if operation == "4":
+    num1 = input("Enter the first number")
+    num2 = input("Enter the second number")
+    print("The quotient is " + str(int(num1) / int(num2)))
+
+input(("close the program"))
+
+   # continuer = True
+    #while continuer:
+        # traitements et calculs
+     #   choix = input("Voulez vous continuer ? (O/N): ")
+      #  if (choix == "n"):
+       #     break
+        #if (choix == "o"):
+
+
+
+
+
+
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1- First, type enter to start. 
+2- Choose between 1,2,3, and 4 to chose the operation.
+3- Then type first and second number to operate with.
+4- Type 2 times enter to close. ( if you're using the command prompt)
